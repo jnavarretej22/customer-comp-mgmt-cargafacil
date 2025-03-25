2b29429e601f9472e0a90d1ca50f07fe98f08a69
janavarretej <janavarretej@telconet.ec>
2025-03-25 14:05:51 -0500

    Cambios servidor 7

---
+++ src/main/resources/db/migration/V3__More_inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO tipo_usuario (nombre_tipo_usuario, descripcion)
+VALUES
+    ('Cliente', 'Usuario tipo cliente'),
+    ('Chofer', 'Usuario tipo chofer'),
+    ('Cooperativa', 'Usuario tipo cooperativa'),
+    ('Administrador', 'Usuario tipo administrador');
+
+
